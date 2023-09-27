f1670f42d67a47fd3683517ac31f3d3f68b7e6e9
sebacastillo <castilloclaudiosebastian@gmail.com>
2023-09-27 07:54:49 -0300

    update gant

--- gant.py
+++ gant.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Define the activities and their durations in weeks
+# Updated activities and durations
 activities = [
     "Revisión de Literatura",
     "Configuración del Entorno en VSCode",
@@ -22,37 +22,32 @@ activities = [
     "Redacción de la Tesis",
     "Revisión y Corrección de la Tesis"
 ]
+durations = [3, 1, 1, 6, 2, 4, 3, 4, 2, 2, 4, 3, 4, 3, 3, 3, 3, 3]
 
-durations = [3, 3, 3, 6, 2, 4, 3, 4, 2, 2, 4, 3, 4, 3, 3, 3, 3, 3]
-
-# Calculate cumulative sum to get the ending week for each task
-cumulative_durations = np.cumsum(durations)
-
-# Invert the order of activities for the Gantt chart
-reversed_activities = activities[::-1]
-reversed_cumulative_durations = np.cumsum(durations[::-1])
-
-# Correct the mistake by reversing both the activities and durations
-# This time, let's keep track of the starting week for each activity to ensure the data aligns correctly
-
+# Create an 'n' array to enumerate the activities
+n = list(range(1, len(activities) + 1))
+n = n[::-1]
 # Initialize the starting week
 start_week = 1
 
-# Create the corrected Gantt chart using Matplotlib
-fig, ax = plt.subplots(figsize=(12, 10))
+# Create the updated Gantt chart with a new 'n' column
+fig, ax = plt.subplots(figsize=(14, 10))
+fig.subplots_adjust(left=0.3)  # Adjust the left margin
 
-# Loop through activities and durations in reversed order
-for i, (activity, duration) in enumerate(zip(reversed(activities), reversed(durations))):
+# Loop through activities, durations, and n in regular (not reversed) order
+for i, (activity, duration, num) in enumerate(zip(activities, durations, n)):
     end_week = start_week + duration - 1  # Calculate the ending week for the current activity
-    ax.broken_barh([(start_week, duration)], (i-0.4, 0.8), facecolors='blue')
-    ax.text(start_week + duration/4, i, f"{start_week}-{end_week}", color="white", fontsize=10)
+    ax.broken_barh([(start_week, duration)], ((len(activities) - i - 1) - 0.4, 0.8), facecolors='blue')
+    ax.text(start_week + duration/4, (len(activities) - i - 1), f"{start_week}-{end_week}", color="white", fontsize=10)
     start_week = end_week + 1  # Update the starting week for the next activity
 
 # Configure the axes and labels
-ax.set_xlabel("Weeks")
+ax.set_xlabel("semanas")
 ax.set_yticks(range(len(activities)))
-ax.set_yticklabels(reversed(activities))
+ax.set_yticklabels([f"{num}. {activity}" for num, activity in zip(n, reversed(activities))])
 ax.grid(True)
 
-plt.title("Corrected Gantt Chart for Master's Thesis Project")
-plt.show()
+plt.title("Gantt - Plan de Trabajo Tesis")
+# Save the Gantt chart as a PNG file
+save_path = 'data/gantt_tesis.png'
+plt.savefig(save_path)
